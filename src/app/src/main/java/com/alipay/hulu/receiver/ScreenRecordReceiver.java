4c20e1f7399ef3579a1b7c268e1550a013ce46b1
xiangleilei <xiangleilei@bbktel.com>
2020-04-20 11:32:28 +0800

    创建录屏广播接收器

---
+++ src/app/src/main/java/com/alipay/hulu/receiver/ScreenRecordReceiver.java
@@ -0,0 +1,15 @@
+package com.alipay.hulu.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ScreenRecordReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
